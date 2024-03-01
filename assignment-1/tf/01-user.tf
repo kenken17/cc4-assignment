2dc5bb133ebf16c0e80f659359bb0b81fd85a580
ken <kenken17@gmail.com>
2024-03-01 21:05:48 +0800

    Rename the resource naming convention

--- assignment-1/tf/01-user.tf
+++ assignment-1/tf/01-user.tf
@@ -3,32 +3,33 @@ provider "aws" {
   profile = "cc4"
 }
 
-resource "aws_iam_user" "provisioner-user" {
-  name = "provisioner-user"
+resource "aws_iam_user" "provisioner_user" {
+  name = "provisioner_user"
+
   tags = {
     Description = "User to provision resources"
   }
 }
 
-resource "aws_iam_group" "provisioner-group" {
-  name = "provisioner-group"
+resource "aws_iam_group" "provisioner_group" {
+  name = "provisioner_group"
 }
 
-resource "aws_iam_policy" "provisioner-policy" {
-  name        = "provisioner-policy"
+resource "aws_iam_policy" "provisioner_policy" {
+  name        = "provisioner_policy"
   description = "provisioner policy"
   policy      = file("./policy/provision-policy.json")
 }
 
-resource "aws_iam_group_policy_attachment" "provisioner-attach" {
-  group      = aws_iam_group.provisioner-group.name
-  policy_arn = aws_iam_policy.provisioner-policy.arn
+resource "aws_iam_group_policy_attachment" "provisioner_attach" {
+  group      = aws_iam_group.provisioner_group.name
+  policy_arn = aws_iam_policy.provisioner_policy.arn
 }
 
-resource "aws_iam_user_group_membership" "provisioner-group-membership" {
-  user = aws_iam_user.provisioner-user.name
+resource "aws_iam_user_group_membership" "provisioner_group_membership" {
+  user = aws_iam_user.provisioner_user.name
 
   groups = [
-    aws_iam_group.provisioner-group.name,
+    aws_iam_group.provisioner_group.name,
   ]
 }
--- assignment-1/tf/02-remote-state.tf
+++ assignment-1/tf/02-remote-state.tf
@@ -1,38 +1,42 @@
-resource "aws_s3_bucket" "state-bucket" {
-  bucket = "cc4-terraform-state-bucket"
+resource "aws_s3_bucket" "state_bucket" {
+  bucket        = "cc4-terraform-state-bucket"
+  force_destroy = true
+
   tags = {
     Description = "S3 Remote State Bucket"
   }
 }
 
-resource "aws_s3_bucket_versioning" "state-bucket-versioning" {
-  bucket = aws_s3_bucket.state-bucket.id
+resource "aws_s3_bucket_versioning" "state_bucket_versioning" {
+  bucket = aws_s3_bucket.state_bucket.id
   versioning_configuration {
     status = "Enabled"
   }
 }
 
-resource "aws_s3_bucket_ownership_controls" "state-bucket-ownership" {
-  bucket = aws_s3_bucket.state-bucket.id
+resource "aws_s3_bucket_ownership_controls" "state_bucket_ownership" {
+  bucket = aws_s3_bucket.state_bucket.id
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
 }
 
-resource "aws_s3_bucket_acl" "state-bucket-acl" {
-  bucket = aws_s3_bucket.state-bucket.id
+resource "aws_s3_bucket_acl" "state_bucket_acl" {
+  bucket = aws_s3_bucket.state_bucket.id
   acl    = "private"
 
-  depends_on = [aws_s3_bucket_ownership_controls.state-bucket-ownership]
+  depends_on = [
+    aws_s3_bucket_ownership_controls.state_bucket_ownership
+  ]
 }
 
-resource "aws_s3_bucket_object_lock_configuration" "state-bucket-object-lock-configuration" {
-  bucket              = aws_s3_bucket.state-bucket.id
+resource "aws_s3_bucket_object_lock_configuration" "state_bucket_object_lock_configuration" {
+  bucket              = aws_s3_bucket.state_bucket.id
   object_lock_enabled = "Enabled"
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "state-bucket-server-side-encryption-configuration" {
-  bucket = aws_s3_bucket.state-bucket.id
+resource "aws_s3_bucket_server_side_encryption_configuration" "state_bucket_server_side_encryption_configuration" {
+  bucket = aws_s3_bucket.state_bucket.id
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
@@ -40,7 +44,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "state-bucket-serv
   }
 }
 
-resource "aws_dynamodb_table" "terraform-lock" {
+resource "aws_dynamodb_table" "terraform_lock" {
   name           = "terraform_state"
   read_capacity  = 5
   write_capacity = 5
@@ -49,7 +53,8 @@ resource "aws_dynamodb_table" "terraform-lock" {
     name = "LockID"
     type = "S"
   }
+
   tags = {
-    "Description" = "DynamoDB Remote State Lock Table"
+    Description = "DynamoDB Remote State Lock Table"
   }
 }
