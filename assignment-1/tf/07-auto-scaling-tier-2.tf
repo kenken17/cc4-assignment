aaa3237265c96ca68154725ca3630c179819a4ca
ken <kenken17@gmail.com>
2024-03-03 17:29:35 +0800

    Add attach for target group and health check

--- assignment-1/tf/07-auto-scaling-tier-2.tf
+++ assignment-1/tf/07-auto-scaling-tier-2.tf
@@ -31,6 +31,10 @@ resource "aws_lb_target_group" "application_tg" {
   port        = 8080
   protocol    = "HTTP"
   vpc_id      = aws_vpc.cc4_vpc.id
+  health_check {
+    path = "/health"
+    port = 8080
+  }
 
   tags = {
     Name = "Application Traget Group"
@@ -111,4 +115,10 @@ resource "aws_autoscaling_group" "appplication_asg" {
   launch_template {
     id = aws_launch_template.application_lt.id
   }
+}
+
+# ALB Target Group attachment
+resource "aws_autoscaling_attachment" "application_attachment" {
+  autoscaling_group_name = aws_autoscaling_group.appplication_asg.id
+  lb_target_group_arn    = aws_lb_target_group.application_tg.arn
 }
